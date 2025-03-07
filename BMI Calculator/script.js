26f269d3117ddcc480bd68bce3eb2e75845b66f4
Swati27-mathur <swatimathur158@gmail.com>
2025-03-07 21:58:02 +0530

    changes in BMI calculator

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,14 +1,49 @@
-
-function bmicalculate(){
+function calculateBMI(){
+    let height = parseFloat(document.getElementById("height").value);
     let weight = parseFloat(document.getElementById("weight").value);
-    let height = parseFloat(document.getElementById("height").value)
-    if (isNaN(weight) || isNaN(height)) {
-        document.getElementById("result").innerText =
-          "please provide all the necessary inputs";
+    let result = document.getElementById("result");
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0){
+        result.innerHTML = "❌ Please enter valid height and weight";
+        result.className = "text-red-400 font-semibold";
         return;
     }
-    let bmicalculate = weight/height;
-    document.getElementById("result").innerText=`BMI Weight is ${bmicalculate}`
+    let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
+    let category ="";
+    if(bmi < 18.5){
+        category = "Underweight";
+    }else if(bmi < 25){
+        category = "Normal weight";
+    }else if(bmi < 30){
+        category = "overweight";
+    }else {
+        category = "Obese";
+    }
+    result.innerHTML = `your bmi is <strong>${bmi}</strong> (${category})`
 }
 
-bmicalculate();
+document.getElementById("calculate").addEventListener("click", calculateBMI)
+
+// function calculateBMI() {
+//     let height = parseFloat(document.getElementById("height").value);
+//     let weight = parseFloat(document.getElementById("weight").value);
+//     let result = document.getElementById("result");
+//     if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+//       result.innerHTML = "❌ Please enter valid height and weight";
+//       result.className = "text-red-400 font-semibold";
+//       return;
+//     }
+//     let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
+//     let category = "";
+//     if (bmi < 18.5) {
+//       category = "Underweight";
+//     } else if (bmi < 25) {
+//       category = "Normal weight";
+//     } else if (bmi < 30) {
+//       category = "Overweight";
+//     } else {
+//       category = "Obese";
+//     }
+//     result.innerHTML = `Your BMI is <strong>${bmi}</strong> (${category})`;
+//   }
+  
+//   document.getElementById("calculate").addEventListener("click", calculateBMI);
